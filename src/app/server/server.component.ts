8d242116199ea3a6b4037928d30818b909c2d0b7
Jun <xjunsantos@gmail.com>
2024-03-10 05:28:34 +0800

    upload files

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'; 
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+    serverId: number = 20;
+    serverStatus: string = 'Online';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+} 
---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: '[app-warning]',
+    templateUrl: './warning.component.html',
+    styles: [`
+        h2 {
+            color: red;
+        }
+    `]
+})
+
+export class WarningComponent {
+
+}
